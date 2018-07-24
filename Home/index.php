14d74ae3362cf90f9c9038ddeeaa9eccb8f3bbef
lucy_glb <lucy_glb@sina.com>
2018-07-24 16:36:05 +0800

    add one js file,a php file to test

---
+++ Home/index.php
@@ -0,0 +1,4 @@
+<?php
+echo 'have a nice day !'
+
+?>
---
+++ hs.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var x = 0;
+  var y = 0;
+  var firecount = null;
+  var balls = new Array("#e597b2", "#e6cde3", "#a6a5c4", "#cd5e3c", "#e09e87", "#cbb994", "#eaf4fc", "#c099a0", "#c3d825", "#a69425", "#f6bfbc", "#c7b370", "#d9333f");
+  var bottom = new Array("#eebbcb", "#cbb994", "#eaf4fc", "#c099a0", "#c7b370", "#d9333f", "#ee827c", "#aa7e4c", "#aa4c8f", "#cd5e3c", "#e09e87");
+  var cannon = "#a2041b",
+  var ballpath = new Array();
+  var ballhide = new Array((160,72)); 
+}
+
